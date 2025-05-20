81a266915297769d6a361d231140a10c54517f12
levi13 <30730638+levi13@users.noreply.github.com>
2025-05-20 17:35:20 -0300

    Update imc_historico_screen.dart

--- lib/screens/imc_historico_screen.dart
+++ lib/screens/imc_historico_screen.dart
@@ -1,92 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../db/database_helper.dart';
 
-class ImcScreen extends StatefulWidget {
-  const ImcScreen({super.key});
+class ImcHistoricoScreen extends StatefulWidget {
+  const ImcHistoricoScreen({super.key});
 
   @override
-  State<ImcScreen> createState() => _ImcScreenState();
+  State<ImcHistoricoScreen> createState() => _ImcHistoricoScreenState();
 }
 
-class _ImcScreenState extends State<ImcScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _alturaController = TextEditingController();
-  final TextEditingController _pesoController = TextEditingController();
-  double? _resultadoIMC;
-  String? _classificacao;
+class _ImcHistoricoScreenState extends State<ImcHistoricoScreen> {
+  List<Map<String, dynamic>> _historico = [];
 
-  Future<void> _calcularERegistrarIMC() async {
-    if (_formKey.currentState!.validate()) {
-      final double altura = double.parse(_alturaController.text);
-      final double peso = double.parse(_pesoController.text);
-      final double imc = peso / (altura * altura);
+  @override
+  void initState() {
+    super.initState();
+    _carregarHistorico();
+  }
 
-      String classificacao;
-      if (imc < 18.5) {
-        classificacao = 'Abaixo do peso';
-      } else if (imc < 25) {
-        classificacao = 'Peso ideal';
-      } else if (imc < 30) {
-        classificacao = 'Sobrepeso';
-      } else {
-        classificacao = 'Obesidade';
-      }
-
-      setState(() {
-        _resultadoIMC = imc;
-        _classificacao = classificacao;
-      });
-
-      final db = await DatabaseHelper().database;
-      await db.insert('imcs', {
-        'altura': altura,
-        'peso': peso,
-        'imc': imc,
-        'data': DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now()),
-      });
-    }
+  Future<void> _carregarHistorico() async {
+    final db = await DatabaseHelper().database;
+    final dados = await db.query('imcs', orderBy: 'data DESC');
+    setState(() {
+      _historico = dados;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Cálculo de IMC')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                controller: _alturaController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Altura (em metros)'),
-                validator: (val) => val!.isEmpty ? 'Informe sua altura' : null,
-              ),
-              TextFormField(
-                controller: _pesoController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Peso (em kg)'),
-                validator: (val) => val!.isEmpty ? 'Informe seu peso' : null,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _calcularERegistrarIMC,
-                child: const Text('Calcular IMC'),
-              ),
-              const SizedBox(height: 20),
-              if (_resultadoIMC != null) ...[
-                Text('IMC: ${_resultadoIMC!.toStringAsFixed(2)}',
-                    style: const TextStyle(fontSize: 18)),
-                Text('Classificação: $_classificacao',
-                    style: const TextStyle(fontSize: 18)),
-              ]
-            ],
-          ),
-        ),
-      ),
+      appBar: AppBar(title: const Text('Histórico de IMC')),
+      body: _historico.isEmpty
+          ? const Center(child: Text('Nenhum registro de IMC encontrado.'))
+          : ListView.builder(
+              itemCount: _historico.length,
+              itemBuilder: (context, index) {
+                final item = _historico[index];
+                return ListTile(
+                  title: Text('IMC: ${item['imc'].toStringAsFixed(2)}'),
+                  subtitle: Text(
+                    'Peso: ${item['peso']} kg, Altura: ${item['altura']} m\nData: ${item['data']}',
+                  ),
+                );
+              },
+            ),
     );
   }
 }
