ff3c9a18c9d87b496257f747704b68dd148c424c
levi13 <30730638+levi13@users.noreply.github.com>
2025-05-20 17:36:30 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/dashboard_screen.dart';
-import 'screens/imc_historico_screen.dart';
+import 'screens/imc_screen.dart';           // tela de cálculo IMC
+import 'screens/imc_historico_screen.dart'; // tela histórico IMC
+import 'screens/lista_refeicoes_screen.dart';
+import 'screens/refeicoes_screen.dart';
+import 'screens/notas_list_screen.dart';
 
 void main() {
   runApp(const AppSaude());
@@ -16,12 +20,15 @@ class AppSaude extends StatelessWidget {
       title: 'Saúde Bahia',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: '/',
+      initialRoute: '/dashboard',
       routes: {
         '/': (context) => const LoginScreen(),
         '/dashboard': (context) => const DashboardScreen(),
         '/imc': (context) => const ImcScreen(),
         '/imc_historico': (context) => const ImcHistoricoScreen(),
+        '/lista_refeicoes': (context) => const ListaRefeicoesScreen(),
+        '/refeicoes':(context) => const RefeicoesScreen(),
+        '/notas': (context) => const NotasListScreen(),
       },
     );
   }
