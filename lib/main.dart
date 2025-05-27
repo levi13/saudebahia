102e5a3f336d19e21eecd3f6536417c92a204091
levi13 <30730638+levi13@users.noreply.github.com>
2025-05-27 18:58:59 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:saudebahia/screens/lista_exercicios_screen.dart';
+import 'package:saudebahia/screens/relatorios_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/dashboard_screen.dart';
 import 'screens/imc_screen.dart';           // tela de cálculo IMC
@@ -6,6 +8,10 @@ import 'screens/imc_historico_screen.dart'; // tela histórico IMC
 import 'screens/lista_refeicoes_screen.dart';
 import 'screens/refeicoes_screen.dart';
 import 'screens/notas_list_screen.dart';
+import 'screens/peso_screen.dart';
+import 'screens/grafico_peso_screen.dart';
+import 'screens/exercicios_screen.dart';
+
 
 void main() {
   runApp(const AppSaude());
@@ -29,6 +35,11 @@ class AppSaude extends StatelessWidget {
         '/lista_refeicoes': (context) => const ListaRefeicoesScreen(),
         '/refeicoes':(context) => const RefeicoesScreen(),
         '/notas': (context) => const NotasListScreen(),
+        '/peso': (context) => const PesoScreen(),
+        '/grafico_peso': (context) => const GraficoPesoScreen(),
+        '/exercicios': (context) => const ExerciciosScreen(),
+        '/lista-exercicios': (context) => const ListaExerciciosScreen(),
+        '/relatorios': (context) => const RelatoriosScreen(),
       },
     );
   }
